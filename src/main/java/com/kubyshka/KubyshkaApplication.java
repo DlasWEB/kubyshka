37db0aa4201470cf337034f881836ee5d28aa82e
Denis Laskin <mr.dlas25@gmail.com>
2023-03-11 23:23:55 +0300

    App start

---
+++ src/main/java/com/kubyshka/KubyshkaApplication.java
@@ -0,0 +1,13 @@
+package com.kubyshka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubyshkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubyshkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubyshka/KubyshkaApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.kubyshka;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KubyshkaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
