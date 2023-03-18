5de5a94ef8f1d4729b6b857cb8d69f3632610d92
Denis Laskin <mr.dlas25@gmail.com>
2023-03-19 02:13:57 +0300

    Add css, js, fonts for Registration and Savings pages

--- src/main/java/com/kubyshka/config/WebSecurityConfig.java
+++ src/main/java/com/kubyshka/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/registration", "/static/**").permitAll()
+                    .antMatchers("/", "/static/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
